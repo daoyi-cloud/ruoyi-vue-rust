9daa4613cfce9c89fa536d985948bd240f39d8a2
Gemiman <913573586@qq.com>
2025-08-30 01:49:10 +0800

    feat(api): 添加全局错误处理和日志记录
    - 在Cargo.toml中添加tracing依赖
    - 在api的create_router函数中增加了对未找到路由和不允许的方法的处理
    - 使用tracing记录相关日志信息

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,16 @@
 use axum::Router;
 use daoyi_common::app::AppState;
+use daoyi_common::app::error::{ApiError, ApiResult};
 
 pub fn create_router() -> Router<AppState> {
-    Router::new().nest("/demo", daoyi_module_demo::api::create_router())
+    Router::new()
+        .nest("/demo", daoyi_module_demo::api::create_router())
+        .fallback(async || -> ApiResult<()> {
+            tracing::warn!("Not Found");
+            Err(ApiError::NotFound)
+        })
+        .method_not_allowed_fallback(async || -> ApiResult<()> {
+            tracing::warn!("Method Not Allowed");
+            Err(ApiError::MethodNotAllowed)
+        })
 }
